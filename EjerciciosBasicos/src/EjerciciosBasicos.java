4444be0bc46b027e5765ce24c81a41844ef091e2
JacielJM <jacimenera@gmail.com>
2022-08-18 21:10:23 -0500

    ejercicios 1, 2 y 3 del repo de alexanyernas

--- EjerciciosBasicos/src/EjerciciosBasicos.java
+++ EjerciciosBasicos/src/EjerciciosBasicos.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class EjerciciosBasicos {
 
@@ -14,7 +15,51 @@ public class EjerciciosBasicos {
 		System.out.println("Práctica 2:");
 		resultado2 = (num5 > num2 && num4 < num3) || (!(num1 == num2) || num3 != num3);
 		System.out.println("La comparación da: "+resultado2);
+		
+		/*
+		 * REPO EJERCICIOS PRÁCTICOS @alexanyernas
+		 */
+		
+		/*
+		 * 1. Escribir un programa que pregunte al usuario por el número de horas
+trabajadas y el coste por hora. Después debe mostrar por la pantalla la
+paga total que le corresponde al usuario.
+		 */
+		Scanner horas, coste;
+		float horasTrabajadas, costeHora, pagaTotal;
+		
+		System.out.println("\nPROGRAMA DE PAGO AL USUARIO");
+		System.out.println("Ingrese el número de horas laboradas:");
+		horas = new Scanner(System.in);
+		horasTrabajadas = horas.nextFloat();
+		System.out.println("Ingrese el pago por hora:");
+		coste = new Scanner(System.in);
+		costeHora = coste.nextFloat();
+		
+		pagaTotal = horasTrabajadas*costeHora;
+		System.out.println("El pago total es de: " + pagaTotal + " MN");
+		
+		horas.close();
+		coste.close();
+		
+		/*
+		 * 2. Escribir un programa que realice la siguiente operación aritmética:
+		 *  ( 3 + 2 ) 
+		 *  ---------
+		 *    2 * 5
+		 */
+		System.out.println("\nOPERACIÓN MATEMÁTICA FRACCIONARIA");
+		float operacion = (float) ((3.0+2.0)/(2.0*5.0));
+		System.out.println("El resultado es: " + operacion);
+		
+		/*
+		 * 3. Escribir un programa que realice la siguiente operación aritmética:
+		 *  raíz cuadrada de [(3.9 + 5.5 * 2.1)^5]
+		 */
+		
+		System.out.println("\nOPERACIÓN MATEMÁTICA RAIZ");
+		double operacion2 = Math.sqrt(Math.pow((3.9+(5.5*2.1)),5));
+		System.out.println("El resultado es: " + operacion2);
+	} // main
 
-	}
-
-}
+} // class
