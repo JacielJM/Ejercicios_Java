f2ceec0650d4b0ec066e8da1531700afbc5bd44f
JacielJM <jacimenera@gmail.com>
2022-08-29 17:12:04 -0500

    Herencia y constuctores

--- POO/src/TestEmployee.java
+++ POO/src/TestEmployee.java
@@ -1,9 +1,30 @@
+import org.generation.nomina.Employee;
 
 public class TestEmployee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Es importante mantener el orden de los parametros al momento de construir a nuestro empleado
+		//Si dejamos el *, tendremos acceso a todos los elementos. En este caso, la unica clase que nos importa es la clase Employee
 
+		Employee Jaciel = new Employee("Jaciel", "Menera", 25, "JJM970203XXX", 400.25);
+		Employee Mariana = new Employee("Mariana", "Valladolid", 28, "xxxxxxxxx", 500.6);
+		
+		System.out.println(Jaciel.toString());
+		System.out.println(Mariana.toString());
+		
+		System.out.println("\nSalario de Jaciel");
+		Jaciel.calculateSalary();
+		
+		System.out.println("\nSalario de Mariana");
+		Mariana.calculateSalary();
+		
+		System.out.println("\nSalario de Mariana enferma");
+		Mariana.calculateSalary(18);
+		
+		System.out.println("\nSalario de Jaciel festivo");
+		Jaciel.calculateSalary(21, 2);
+		
 	}
 
 }
--- POO/src/org/generation/nomina/Employee.java
+++ POO/src/org/generation/nomina/Employee.java
@@ -9,6 +9,25 @@ public class Employee {
 	public String RFC;
 	public double salary;
 	
+	public Employee(String firstName, String lastName, int age, String rfc, double salary) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.RFC = rfc;
+		this.salary = salary;
+	}
+	
+	public Employee(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public Employee(String firstName, int age) {
+		this.firstName = firstName;
+		this.age = age;
+	}
+	
+	
 	//Metodos (acciones a realizar)
 	
 		//Obtener el nombre
@@ -19,8 +38,35 @@ public class Employee {
 		//Metodo para calcular el salario
 		//Sintaxis del metodo (modificador de acceso + void + nombre del metodo + parametros)
 	
-	public void calculateSalary() {
-		System.out.println(salary*21);
+	//Estructura de un metodo o una funcion
+	
+		//modificador de acceso + tipo de retorno + Nombre + Parametros
+	
+	//Método para calcular salario
+	public void calculateSalary () {
+		System.out.println("El salario es: " + this.salary*21);
 	}
+	
+	public void calculateSalary (int days) {
+		System.out.println("El salario es: " + this.salary*days);
+	}
+	
+	//Método para calcular salario con dias festivos
+	public void calculateSalary (int days, int holidays) {
+		holidays *=3;
+		System.out.println("Trabajaste un día feriado, te pago el triple");
+		System.out.println((this.salary*holidays)+(this.salary*days));
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", RFC=" + RFC
+				+ ", salary=" + salary + "]";
+	}
+
+	
+
+	
+	
 
 }
