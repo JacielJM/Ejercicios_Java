ac4561809d468b3d123fdb335e0980706ad95267
JacielJM <jacimenera@gmail.com>
2022-09-19 17:16:56 -0500

    proyectos spring boot y gradle

---
+++ ferrepsgdb/src/main/java/com/generation/ferrepsgdb/FerrepsgdbApplication.java
@@ -0,0 +1,13 @@
+package com.generation.ferrepsgdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerrepsgdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerrepsgdbApplication.class, args);
+	}
+
+}
